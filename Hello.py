b847d56f7d2065031cf895be1d3e5cc78d74222d
Franca <39587464+Franca3@users.noreply.github.com>
2023-12-14 15:06:39 +0100

    makes int instead of strings

--- Hello.py
+++ Hello.py
@@ -1,2 +1,19 @@
-print('hello world')
+import sys;
 
+class adreslist:
+    def readinput():
+        f = open("Documents/GitHub/Hackathon-Christmas/Datasets/1_Geldermalsen.txt", 'r+')
+        listinput = []
+        for line in f.readlines():
+            listinput.append(line)
+        f.close()
+        print(listinput)
+    readinput()
+    
+
+
+
+
+#class elf:
+
+#class route:
